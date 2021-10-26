f47fa6e36966cbe69387f7c48e4bc60adb179f8a
maruf <marufsharifi97103@gmail.com>
2021-10-26 15:31:32 +0430

    original routes are created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import {Home} from "./home/home";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <nav className="nav">
+          <ul>
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/destination">DESTINATION</Link>
+            </li>
+            <li>
+              <Link to="/crew">CREW</Link>
+            </li>
+            <li>
+              <Link to="/technology">TECHNOLOGY</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/destination">
+            <div></div>
+          </Route>
+          <Route exact path="/crew">
+            <div></div>
+          </Route>
+          <Route exact path="/technology">
+            <div></div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
