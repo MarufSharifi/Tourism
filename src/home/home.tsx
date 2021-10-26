65f684a6ef975299a5aa23cc9ff663532bbe0929
maruf <marufsharifi97103@gmail.com>
2021-10-26 15:30:11 +0430

    home page partially implemented

---
+++ src/home/home.tsx
@@ -0,0 +1,25 @@
+import "./home.css";
+
+export const Home = () => {
+  return (
+    <div className="container">
+      <div className="subContainer">
+        <text className="title">SO, YOU WANT TO TRAVEL TO</text>
+        <div className="content">
+          <div className="descriptionContainer">
+            <text className="spaceText">SPACE</text>
+            <text className="descriptionText">
+              let's face it; if you want to go to space, you might as well
+              genuinely go to outer space and not over kind of one the age of
+              it. Well sit back, and relax because we'll give you a truly out of
+              this world experience
+            </text>
+          </div>
+          <div className="exploreContainer">
+            <text className="exploreText">EXPLORE</text>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
