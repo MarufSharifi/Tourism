77fb13c4dd8451de068ca8d71bdb8c7a5b37e39e
maruf <marufsharifi97103@gmail.com>
2021-10-30 11:51:21 +0430

    used original text instead of dummy texts

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -4,15 +4,15 @@ export const Home = () => {
   return (
     <div className="container">
       <div className="subContainer">
-        <text className="title">SO, YOU WANT TO TRAVEL TO</text>
+        <text className="title">So, you want to travel to</text>
         <div className="content">
           <div className="descriptionContainer">
-            <text className="spaceText">SPACE</text>
+            <text className="spaceText">Space</text>
             <text className="descriptionText">
-              let's face it; if you want to go to space, you might as well
-              genuinely go to outer space and not over kind of one the age of
-              it. Well sit back, and relax because we'll give you a truly out of
-              this world experience
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
             </text>
           </div>
           <div className="exploreContainer">
