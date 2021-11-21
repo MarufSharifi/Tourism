42805441be9dc42ba8c97c20a60fd179a4dcb5bd
maruf <marufsharifi97103@gmail.com>
2021-11-21 12:29:21 +0430

    upgraded to react-router-dom6 in desination route

--- src/destination/destination.tsx
+++ src/destination/destination.tsx
@@ -1,48 +1,34 @@
-import {
-  BrowserRouter as Router,
-  Link,
-  Switch,
-  Route,
-  useRouteMatch,
-} from "react-router-dom";
-import {Details} from "./details";
+import {Link, Outlet} from "react-router-dom";
 
 export const Destination = () => {
-  const {path, url} = useRouteMatch();
-
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ol>
-            <li>
-              <Link className="link" to={`${url}/moon`}>
-                moon
-              </Link>
-            </li>
-            <li>
-              <Link className="link" to={`${url}/mars`}>
-                mars
-              </Link>
-            </li>
-            <li>
-              <Link className="link" to={`${url}/europe`}>
-                europe
-              </Link>
-            </li>
-            <li>
-              <Link className="link" to={`${url}/titan`}>
-                titan
-              </Link>
-            </li>
-          </ol>
-        </nav>
-      </div>
-      <Switch>
-        <Route path={`${path}/destination`}>
-          <Details />
-        </Route>
-      </Switch>
-    </Router>
+    <div className="App">
+      <nav>
+        <ol>
+          <li>
+            <Link className="link" to={`/destination`}>
+              moon
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to={`/mars`}>
+              mars
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to={`/europe`}>
+              europe
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to={`/titan`}>
+              titan
+            </Link>
+          </li>
+          Link
+        </ol>
+      </nav>
+      <Outlet />
+    </div>
   );
 };
--- src/destination/details.tsx
+++ src/destination/details.tsx
@@ -1,7 +1,11 @@
+import {useParams} from "react-router-dom";
+
 export const Details = () => {
+  const {id} = useParams();
+
   return (
     <div>
-      <div>you are welcome</div>
+      <div>{`you are welcome ${id}`}</div>
     </div>
   );
 };
