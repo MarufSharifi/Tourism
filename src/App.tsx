856b158c94fa432626edc25bed2209923551ba9f
maruf <marufsharifi97103@gmail.com>
2021-11-21 12:28:06 +0430

    Configured react-router-dom6

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,53 @@
-import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import {Route, Link, Routes} from "react-router-dom";
 import {Home} from "./home/home";
+import {Details} from "./destination/details";
 import {Destination} from "./destination/destination";
 import Logo from "./assets/logo";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div className="header">
-          <Logo className="logo" />
-          <nav>
-            <div className="headerLine" />
-            <div className="navRight">
-              <div className="lineOnNav" />
-              <ol>
-                <li>
-                  <Link className="link" to="/">
-                    00 home
-                  </Link>
-                </li>
-                <li>
-                  <Link className="link" to="/destination/moon">
-                    01 destination
-                  </Link>
-                </li>
-                <li>
-                  <Link className="link" to="/crew">
-                    02 crew
-                  </Link>
-                </li>
-                <li>
-                  <Link className="link" to="/technology">
-                    03 technology
-                  </Link>
-                </li>
-              </ol>
-            </div>
-          </nav>
-        </div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/destination">
-            <Destination />
-          </Route>
-          <Route exact path="/crew">
-            <div></div>
-          </Route>
-          <Route exact path="/technology">
-            <div></div>
-          </Route>
-        </Switch>
+    <div className="App">
+      <div className="header">
+        <Logo className="logo" />
+        <nav>
+          <div className="headerLine" />
+          <div className="navRight">
+            <div className="lineOnNav" />
+            <ol>
+              <li>
+                <Link className="link" to="/">
+                  00 home
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="/destination">
+                  01 destination
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="/crew">
+                  02 crew
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="/technology">
+                  03 technology
+                </Link>
+              </li>
+            </ol>
+          </div>
+        </nav>
       </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />}>
+          <Route path=":id" element={<Details />} />
+        </Route>
+        <Route path="/crew" element={<div />} />
+        <Route path="/technology" element={<div />} />
+      </Routes>
+    </div>
   );
 }
 
